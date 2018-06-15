bb8d2fd2e2af84e1fbc6be9fe6edfd29f75c3fcc
Jason Rymal <jasonrymal@gmail.com>
2018-06-14 22:49:03 -0400

    missed something else in there

--- online5eCS-serviceworker.js
+++ online5eCS-serviceworker.js
@@ -64,6 +64,6 @@ self.addEventListener('fetch', function(event) {
                         .catch(function(error) {console.log('Failed to extract cache: ' + error);})
                 })
                 .catch(function(error) {console.log('Registration failed with ' + error);})
-        });
+        })
     );
 });
