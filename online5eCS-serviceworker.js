fafc7aee1a21b7d7d7af9085d59ef0378d9a71ef
Jason Rymal <jasonrymal@gmail.com>
2018-06-14 22:57:14 -0400

    new add was missing all the other files loaded as they were file cache already

--- online5eCS-serviceworker.js
+++ online5eCS-serviceworker.js
@@ -1,38 +1,25 @@
 const CACHE_NAME = 'online5eCS';
 
 self.addEventListener('install', function(event) {
-    var indexPage = new Request('index.html');
     event.waitUntil(
-        fetch(indexPage)
-        .then(function(response) {
-            return caches.open(CACHE_NAME)
-                .then(function(cache) {
-                    console.log(CACHE_NAME+': Cached index page during Install'+ response.url);
-                    return cache.put(indexPage, response);
-                })
-                .catch(function(error) {console.log('Failed openning cache: ' + error);});
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll([
+                '/online5eCS/',
+                '/online5eCS/index.html',
+                '/online5eCS/fallback.html',
+                '/online5eCS/manifest.json',
+                '/online5eCS/js/Backstories.js',
+                '/online5eCS/js/Classes.js',
+                '/online5eCS/js/GameEngine.js',
+                '/online5eCS/js/Init.js',
+                '/online5eCS/js/Races.js',
+                '/online5eCS/js/Skills.js',
+                '/online5eCS/css/core.css',
+                '/online5eCS/images/icons-192.png',
+                '/online5eCS/images/icons-512.png'
+            ]);
         })
         .catch(function(error) {console.log('Failed fetching cache: ' + error);}));
-
-   /* e.waitUntil(
-   caches.open('online5eCS').then(function(cache) {
-     return cache.addAll([
-       '/online5eCS/',
-       '/online5eCS/index.html',
-       '/online5eCS/fallback.html',
-       '/online5eCS/manifest.json',
-       '/online5eCS/js/Backstories.js',
-       '/online5eCS/js/Classes.js',
-       '/online5eCS/js/GameEngine.js',
-       '/online5eCS/js/Init.js',
-       '/online5eCS/js/Races.js',
-       '/online5eCS/js/Skills.js',
-       '/online5eCS/css/core.css',
-       '/online5eCS/images/icons-192.png',
-       '/online5eCS/images/icons-512.png'
-     ]);
-   })
- );*/
 });
 
 self.addEventListener('fetch', function(event) {
