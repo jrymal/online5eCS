425f5a28804d61e2313453358aac1197d2f38c1d
Jason Rymal <jasonrymal@gmail.com>
2018-06-14 23:40:32 -0400

    stupid user....more trials on the worker

--- online5eCS-serviceworker.js
+++ online5eCS-serviceworker.js
@@ -4,19 +4,19 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(CACHE_NAME).then(function(cache) {
             return cache.addAll([
-                '/online5eCS/',
-                '/online5eCS/index.html',
-                '/online5eCS/fallback.html',
-                '/online5eCS/manifest.json',
-                '/online5eCS/js/Backstories.js',
-                '/online5eCS/js/Classes.js',
-                '/online5eCS/js/GameEngine.js',
-                '/online5eCS/js/Init.js',
-                '/online5eCS/js/Races.js',
-                '/online5eCS/js/Skills.js',
-                '/online5eCS/css/core.css',
-                '/online5eCS/images/icons-192.png',
-                '/online5eCS/images/icons-512.png'
+                '',
+                'index.html',
+                'fallback.html',
+                'manifest.json',
+                'js/Backstories.js',
+                'js/Classes.js',
+                'js/GameEngine.js',
+                'js/Init.js',
+                'js/Races.js',
+                'js/Skills.js',
+                'css/core.css',
+                'images/icons-192.png',
+                'images/icons-512.png'
             ]);
         })
     );
