5935d08321187a7fafad3bee881bfbc7ad28e271
Jason Rymal <jasonrymal@gmail.com>
2018-06-14 21:30:37 -0400

    i am starting to give up here

--- js/online5eCS-serviceworker.js
+++ js/online5eCS-serviceworker.js
@@ -2,19 +2,19 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('online5eCS').then(function(cache) {
      return cache.addAll([
-       'online5eCS/',
-       'online5eCS/index.html',
-       'online5eCS/fallback.html',
-       'online5eCS/manifest.json',
-       'online5eCS/js/Backstories.js',
-       'online5eCS/js/Classes.js',
-       'online5eCS/js/GameEngine.js',
-       'online5eCS/js/Init.js',
-       'online5eCS/js/Races.js',
-       'online5eCS/js/Skills.js',
-       'online5eCS/css/core.css',
-       'online5eCS/images/icons-192.png',
-       'online5eCS/images/icons-512.png'
+       '/online5eCS/',
+       '/online5eCS/index.html',
+       '/online5eCS/fallback.html',
+       '/online5eCS/manifest.json',
+       '/online5eCS/js/Backstories.js',
+       '/online5eCS/js/Classes.js',
+       '/online5eCS/js/GameEngine.js',
+       '/online5eCS/js/Init.js',
+       '/online5eCS/js/Races.js',
+       '/online5eCS/js/Skills.js',
+       '/online5eCS/css/core.css',
+       '/online5eCS/images/icons-192.png',
+       '/online5eCS/images/icons-512.png'
      ]);
    })
  );
